9a1f7d541ff59d98b341e69648fcc640e59425bb
juba <juliano.pedraca@gmail.com>
2025-04-15 00:03:42 -0300

    fix-lint

--- resources/js/layouts/app/app-header-layout.tsx
+++ resources/js/layouts/app/app-header-layout.tsx
@@ -3,7 +3,7 @@ import { AppShell } from '@/components/app-shell';
 import { type BreadcrumbItem } from '@/types';
 import type { PropsWithChildren } from 'react';
 
-export default function AppHeaderLayout({ children, breadcrumbs }: PropsWithChildren<{ breadcrumbs?: BreadcrumbItem[] }>) {
+export default function AppHeaderLayout({ children }: PropsWithChildren<{ breadcrumbs?: BreadcrumbItem[] }>) {
     return (
         <AppShell>
             <AppContent>{children}</AppContent>
